edb8fa2924478463271571090f99fb97ef536cea
Bartek <bartlomiejwaszak1@gmail.com>
2019-09-30 20:02:57 +0200

    updated cv & project-page

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -3,7 +3,9 @@ import React from 'react';
 const ProjectPage = () => {
     return (
         <>
-            work in progress!
+            <div className="project__text-1">If you want to see my projects, go to my github!</div>
+            <div ><a className="project__icon-github" href="https://github.com/Bartek8"><i class="fab fa-github"></i></a></div>
+            <div className="project__text-2">In the future I will add projects here.</div>
         </>);
 }
 
